6c535a32181e28bbcd4346283eba3c6a7bbb8813
Daniel Lawrence <daniel@danielscottlawrence.com>
2012-05-10 20:28:37 +1000

    added awesome lists.py

---
+++ lists.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python
+
+
+# add an item to the front of a list
+abcs = [ 'b','c','d' ]
+print abcs
+# prints ['b', 'c', 'd']
+
+# use insert to insert to position 0(ZERO)
+abcs.insert(0, 'a' )
+print abcs
+# prints ['a', 'b', 'c', 'd']
