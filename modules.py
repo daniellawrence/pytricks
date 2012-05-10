7e34589f6b5b7b6fa92981296d6f415462bc2605
Daniel Lawrence <daniel@danielscottlawrence.com>
2012-05-10 20:23:24 +1000

    updated modules.py with some help comments

--- modules.py
+++ modules.py
@@ -5,20 +5,28 @@ import types
 def get_docstrings():
     """ get all the docstrings for a module """
     docstrings = {}
+    # loop over everything inside the module_name 
     for func_name in module_name.__dict__.keys():
+        # grab an instance of the item
         func = module_name.__dict__[func_name]
+        # check if the instance is a function
         if not isinstance(func, types.FunctionType ):
 		continue
+        # if it is then pull out the __doc__ and add it othe dict
         docstrings[ func_name ] = module_name.__dict__[func_name].__doc__
     return docstrings
 #------------------------------------------------------------------------------
 def get_functions():
     """ get all the functions for a module """
     function_list = []
+    # loop over everything inside the module_name 
     for func_name in module_name.__dict__.keys():
+        # grab an instance of the item
         func = module_name.__dict__[func_name]
+        # check if the instance is a function
         if not isinstance(func, types.FunctionType ):
 		continue
+        # if it is then append it to our list
         function_list.append(func_name)
     return function_list
 
